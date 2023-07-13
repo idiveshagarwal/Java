11f8c3f97730e1c1901aa7910ff0f986007f29d4
Divesh Agarwal <diveshagarwal1995@gmail.com>
2023-07-13 09:41:44 +0530

    java practice

---
+++ 12-if-else.java
@@ -0,0 +1,18 @@
+/* if (condition-to-be-checked) {
+    statements-if-condition-true;
+}
+else {
+    statements-if-condition-false;
+} */
+
+public class if_else {
+    public static void main(String[] args) {
+        int a = 29;
+        if (a > 18) {
+            System.out.println("You can drive");
+        }
+        else {
+            System.out.println("You are underage!");
+        }
+    }
+}
