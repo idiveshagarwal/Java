4ac46b6d1babee6fcd6f361da214860751ab5df1
Divesh Agarwal <diveshagarwal1995@gmail.com>
2023-09-11 11:01:44 +0530

    java practice

--- 75 - CalendarClass.java
+++ 75 - CalendarClass.java
@@ -1,28 +1,23 @@
-Code Name: Get Calendar Type
-
 import java.util.*;
 
-public class GetCalendarType {
+public class CalendarExample {
     public static void main(String[] args) {
         Calendar c = Calendar.getInstance();
-        System.out.println(c.getCalendarType()); // getCalendarType() returns the type of the calendar
+        System.out.println(c.getCalendarType());  // getCalendarType() returns the type of the calendar
     }
 }
 
-Output:
-gregory
+Output: gregory
 
-Description: This Java program retrieves the calendar type using the `getCalendarType()` method from a `Calendar` instance.
-
-Code Name: Get Various Calendar Fields
+Description: This code defines a class named `CalendarExample` and uses the `Calendar` class to get the type of the calendar system in use (e.g., "gregory").
 
 import java.util.*;
 
-public class GetVariousCalendarFields {
+public class CalendarExample {
     public static void main(String[] args) {
         Calendar c = Calendar.getInstance();
         System.out.println("Current year is: " + c.get(Calendar.YEAR));
-        System.out.println("Current month is: " + (c.get(Calendar.MONTH) + 1)); // Adjust for 0-based month indexing
+        System.out.println("Current month is: " + (c.get(Calendar.MONTH) + 1)); // The indexing for month field ranges from [0,11]
         System.out.println("Current day is: " + c.get(Calendar.DAY_OF_WEEK));
         System.out.println("Current hour is: " + c.get(Calendar.HOUR_OF_DAY));
         System.out.println("Current minute is: " + c.get(Calendar.MINUTE));
@@ -31,69 +26,50 @@ public class GetVariousCalendarFields {
 }
 
 Output:
+
 Current year is: 2023
 Current month is: 9
-Current day is: 2
-Current hour is: 14
-Current minute is: 30
+Current day is: 1
+Current hour is: 0
+Current minute is: 0
 Current second is: 0
 
-Description: This Java program retrieves various calendar fields such as year, month, day of the week, hour, minute, and second using the `get()` method from a `Calendar` instance, adjusting for zero-based month indexing.
+Description: This code retrieves various date and time components using the `Calendar` class and prints them out. Note that the month index is adjusted by 1 because it is 0-based.
 
-Code Name: Add and Subtract from Calendar Fields
 import java.util.*;
 
-public class AddAndSubtractFromCalendarFields {
+public class CalendarExample {
     public static void main(String[] args) {
         Calendar c = Calendar.getInstance();
         System.out.println("Current date is: " + c.getTime());
-        
         c.add(Calendar.YEAR, 4);
         System.out.println("After 4 years: " + c.getTime());
-        
         c.add(Calendar.YEAR, -12);
         System.out.println("Before 12 years: " + c.getTime());
-        
         c.add(Calendar.MONTH, 2);
         System.out.println("After 2 months: " + c.getTime());
     }
 }
 
 Output:
-Current date is: Sat Sep 04 14:30:00 IST 2023
-After 4 years: Wed Sep 04 14:30:00 IST 2027
-Before 12 years: Fri Sep 04 14:30:00 IST 2015
-After 2 months: Sun Nov 04 14:30:00 IST 2015
 
-Description: This Java program demonstrates how to add and subtract years and months from a `Calendar` instance's date using the `add()` method.
+Current date is: Sun Sep 11 00:00:00 IST 2023
+After 4 years: Wed Sep 11 00:00:00 IST 2027
+Before 12 years: Thu Sep 11 00:00:00 IST 2015
+After 2 months: Sat Nov 11 00:00:00 IST 2015
 
-Code Name: Get Weeks in Week Year
+
+Description: This code uses the `Calendar` class to manipulate dates by adding and subtracting years and months, and it prints out the resulting dates.
 
 import java.util.*;
 
-public class GetWeeksInWeekYear {
+public class CalendarExample {
     public static void main(String[] args) {
         Calendar c = Calendar.getInstance();
-        System.out.println(c.getWeeksInWeekYear());
+        System.out.println("The maximum number of weeks in a year: " + c.getMaximum(Calendar.WEEK_OF_YEAR));
     }
 }
 
-Output:
-52
+Output: The maximum number of weeks in a year: 53
 
-Description: This Java program calculates and prints the number of weeks in the current calendar year using the `getWeeksInWeekYear()` method.
-
-Code Name: Get Maximum Weeks in a Year
-import java.util.*;
-
-public class GetMaximumWeeksInYear {
-    public static void main(String[] args) {
-        Calendar c = Calendar.getInstance();
-        System.out.println("The maximum no. of weeks in a year: " + c.getMaximum(Calendar.WEEK_OF_YEAR));
-    }
-}
-
-Output:
-The maximum no. of weeks in a year: 53
-
-Description: This Java program retrieves and prints the maximum number of weeks in a year using the `getMaximum(Calendar.WEEK_OF_YEAR)` method from a `Calendar` instance.
+Description: This code retrieves and prints the maximum number of weeks in a year according to the calendar system in use.
