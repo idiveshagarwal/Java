e4a4f2496b74fbd8a8f8b33d95e27ee3248d7757
Divesh Agarwal <diveshagarwal1995@gmail.com>
2023-09-18 19:02:32 +0530

    java practice

---
+++ 83 - JavaDocUsageDemonstration.java
@@ -0,0 +1,16 @@
+public class DIV {
+    /**
+     * This method is used to return the division of two numbers
+     * @throws ArithmeticException if divided by 0
+     * @param a First parameter - Integer
+     * @param b Second parameter - Integer
+     * @return Integer
+     */
+    public int division(int a, int b){
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("This is my main method");
+    }
+}
